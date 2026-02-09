ef2f33bf709acfe5a600abed79a74e27abe55f17
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-09 15:34:36 +0100

    import controller delle funzioni e associazione alle varie rotte + test postman

--- routers/posts.js
+++ routers/posts.js
@@ -1,60 +1,34 @@
 //raggruppo tutte le rotte che iniziano con /posts in un file e lo uso in 
 // app.js tramite la classe express.Router()
 
-
 // richiamo istanza di framework Express
 const express = require('express')
 // creiamo un istanza dell'oggetto rotte di Express
 const router = express.Router();
 
-// importo i dati dei post
-const postsList = require('./../data/postsArr');
-
+//import del controller
+const blogController = require('./../controllers/blogController')
 
 
 // rotta GET /posts
-router.get('/', function (req, res) {
-
-   //restituisco l'oggetto
-    res.json(postsList);
-
-   
-});
+router.get('/',blogController.index);
 
 //inizio crud 
 
 //SHOW
-router.get('/:id', function (req, res) {
-    // res.send('Dettagli del post con id ' + req.params.id); 
-
-    //prova restituzione singolo post dalla rotta show in formato json
-    //recupero id da url ( mi salvo l'id inserito in una variabile)
-    const postId = parseInt(req.params.id);
-
-    //confronto l'id salvato con gli id presenti nell'array POSTList 
-    const post = postsList.find(post => post.id === postId);
-    res.json(post);
-});
+router.get('/:id', blogController.show);
 
 // CREATE ( STORE)
-router.post('/', function (req, res) {
-    res.send('Creazione nuovo post');
-});
+router.post('/',blogController.store);
 
 // UPDATE
-router.put('/:id', function (req, res) {
-    res.send('Modifica del post con id ' + req.params.id);
-});
+router.put('/:id', blogController.update);
 
 // MODIFY
-router.patch('/:id', function (req, res) {
-    res.send('Modifica parziale del post ' + req.params.id);
-});
+router.patch('/:id', blogController.update);
 
 // DELETE
-router.delete('/:id', function (req, res) {
-    res.send('Eliminazione del post ' + req.params.id);
-});
+router.delete('/:id', blogController.destroy);
 
 // esporta l'istanza di queste rotte
 module.exports = router;
