fb5813b6b48fc449a2c714db8e5979d57eb566fa
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-09 16:15:03 +0100

    aggiunto controllo se esiste il post altrimenti rispondi con uno stato 404 in show() e destroy()

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -3,62 +3,85 @@ const postsArr = require('./../data/postsArr');
 
 
 function index(req, res) {
- 
-   //inizialmente, il blog filtrato corrisponde a quello originale
-   let filteredPost = postsArr; 
-   
-   //se la richiesta contiene un filtro, allora filtro l'array
-   if(req.query.tags) {
-    filteredPost = postsArr.filter(
-        blog => blog.tags.includes(req.query.tags)
-    ); 
-   }
 
- //creo un nuovo oggetto con le propr che mi servono
- const oggettoBlog = {
-    numeroPost : postsArr.length,
-    listaPost : filteredPost
- }
+    //inizialmente, il blog filtrato corrisponde a quello originale
+    let filteredPost = postsArr;
+
+    //se la richiesta contiene un filtro, allora filtro l'array
+    if (req.query.tags) {
+        filteredPost = postsArr.filter(
+            blog => blog.tags.includes(req.query.tags)
+        );
+    }
+
+    //creo un nuovo oggetto con le propr che mi servono
+    const oggettoBlog = {
+        numeroPost: postsArr.length,
+        listaPost: filteredPost
+    }
     res.json(oggettoBlog);
 }
 
 function show(req, res) {
 
- //recupero l'id e lo trasformo in numero
- const idNum = parseInt(req.params.id) 
+    //recupero l'id e lo trasformo in numero
+    const idNum = parseInt(req.params.id)
 
- //cerco il post tramite id
- const post = postsArr.find(blog => blog.id === idNum)
+    //cerco il post tramite id
+    const post = postsArr.find(blog => blog.id === idNum);
 
- //restituisco sotto forma di JSON 
- res.json(post)
+    //controllo se trova l'item
+    if (!post) {
+
+        res.status(404);
+
+        //risposta con messaggio di eerrore
+        return res.json({
+            error: "Not found",
+            message: "Post non trovato"
+        })
+    }
+
+    //restituisco sotto forma di JSON 
+    res.json(post)
 }
 
 function store(req, res) {
 
-res.send('Creazione nuovo post');
+    res.send('Creazione nuovo post');
 
 }
 
 function update(req, res) {
 
-res.send('Modifica del post con id ' + req.params.id);
+    res.send('Modifica del post con id ' + req.params.id);
 
 }
 
 function destroy(req, res) {
 
-//recupero l'id e lo trasformo in numero
- const idNum = parseInt(req.params.id) ;
+    //recupero l'id e lo trasformo in numero
+    const idNum = parseInt(req.params.id);
 
-  //cerco il post tramite id
- const post = postsArr.find(blog => blog.id === idNum)
+    //cerco il post tramite id
+    const post = postsArr.find(blog => blog.id === idNum)
 
- //rimuovo il post dal blog 
-postsArr.splice(postsArr.indexOf(post), 1);
+    //controllo se trova l'item
+    if (!post) {
 
-//risposta 
-res.sendStatus(204);
+        res.status(404);
+
+        //risposta con messaggio di eerrore
+        return res.json({
+            error: "Not found",
+            message: "Post non trovato"
+        })
+    }
+    //rimuovo il post dal blog 
+    postsArr.splice(postsArr.indexOf(post), 1);
+
+    //risposta 
+    res.sendStatus(204);
 
 }
 
